056add4fb75c9231a3268207897a9250b15b8eb3
loic_lefevre <loic.lefevre@oracle.com>
2025-04-22 17:28:29 +0200

    test ga 0.0.23

--- .github/build-github.sh
+++ .github/build-github.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "Managing Oracle Autonomous Database..."
-export INFO=$(curl -s -k -L -X GET "https://api.testpilot-controller.oraclecloud.com/ords/testpilot/admin/database?type=autonomous" -H 'accept: application/json')
-export HOST=$(echo $INFO | jq -r '.database' | jq -r '.host')
-export SERVICE=$(echo $INFO | jq -r '.database' | jq -r '.service')
 goal="-DdbUser=test_$RUNID -DdbPassword=My_Password_4_Testing -DrunID=$RUNID -DdbHost=$HOST -DdbService=$SERVICE"
 
 function logAndExec() {
