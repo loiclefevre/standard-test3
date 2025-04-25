c1003031a66786e74ffa03e9bb02e0e1dbc4949b
loic_lefevre <loic.lefevre@oracle.com>
2025-04-25 16:31:27 +0200

    test ga 0.0.35

--- src/test/java/org/datawarriors/standard/ExampleTest.java
+++ src/test/java/org/datawarriors/standard/ExampleTest.java
@@ -19,7 +19,7 @@ public class ExampleTest {
 				try (ResultSet r = s.executeQuery("select 'Hello from database!' from dual")) {
 					if (r.next()) {
 						try {
-							Thread.sleep( 1002L ); // 1 second
+							Thread.sleep( 1003L ); // 1 second
 						}
 						catch (InterruptedException ignored) {
 						}
