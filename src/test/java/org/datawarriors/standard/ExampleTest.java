17c7a3bccc4b2670c857dc1cf996b2bba3e02999
loic_lefevre <loic.lefevre@oracle.com>
2025-04-30 10:59:45 +0200

    test ga 0.0.49

--- src/test/java/org/datawarriors/standard/ExampleTest.java
+++ src/test/java/org/datawarriors/standard/ExampleTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 public class ExampleTest {
 	@Test
 	void quickTest() {
+		System.out.println(System.getProperty("jdbc.url"));
 		try (Connection c = DriverManager.getConnection(System.getProperty("jdbc.url"))) {
 			try (Statement s = c.createStatement()) {
 				try (ResultSet r = s.executeQuery("select 'Hello from database!' from dual")) {
