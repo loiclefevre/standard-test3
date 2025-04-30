66a3a5555b41d92d59d9ed5b4af47342046388c0
loic_lefevre <loic.lefevre@oracle.com>
2025-04-30 11:53:04 +0200

    test ga 0.0.49

--- .github/build-github.sh
+++ .github/build-github.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-goal="-DdbUser=test_$RUNID -DdbPassword=My_Password_4_Testing -DrunID=$RUNID -DdbHost=$HOST -DdbService=$SERVICE"
+goal="-Drdbms=$RDBMS -DdbUser=test_$RUNID -DdbPassword=My_Password_4_Testing -DrunID=$RUNID -DdbHost=$HOST -DdbService=$SERVICE"
 
 function logAndExec() {
   echo 1>&2 "Executing:" "${@}"
   exec "${@}"
 }
-
-echo "DIR: `pwd`"
-echo $PATH
-echo $JAVA_HOME
-ls -palrt ./gradle/
 
 logAndExec ./gradlew test ${goal} "${@}" -Plog-test-progress=true --stacktrace
